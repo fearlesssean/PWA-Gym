8c42dc9e9fb59d882f1b27deaf046fd40d72c579
fearlesssean <76848318+fearlesssean@users.noreply.github.com>
2025-01-03 11:07:16 -0500

    Update service-worker.js
    Updated correct PATHS

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,43 @@
+const CACHE_NAME = 'PWA-Gym-cache-v1';
+const urlsToCache = [
+  '/PWA-Gym/',
+  '/PWA-Gym/index.html',
+  '/PWA-Gym/manifest.json',
+  '/PWA-Gym/icons/icon-192x192.png',
+  '/PWA-Gym/icons/icon-512x512.png'
+];
+
+// Install event: Cache app resources
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('pwa-cache').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/manifest.json',
-                '/icons/icon-192x192.png',
-                '/icons/icon-512x512.png'
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
+// Fetch event: Serve cached resources or fetch from network
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            return cachedResponse || fetch(event.request);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
+// Activate event: Clean up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      )
+    )
+  );
 });
